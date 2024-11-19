1bbaf8357427463a9abb607dcf17293e27948a99
Sacha <sacha.v@hotmail.be>
2024-11-19 14:35:54 +0100

    Ajout de commentaires

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
 function validate() {
     event.preventDefault()
-    let badAnswer = 0
-    const info = document.getElementById('pInfoBottom')
+    let badAnswer = 0   //On compte les mauvaises réponses pour l'affichage final
+    const info = document.getElementById('pInfoBottom') //Div contenant l'affiche final
     const body = document.getElementById('body')
     for (i = 1 ; i < 6 ; i++) {
-        const result = document.querySelector('input[name="q'+i+'"]:checked')
-        const pAnswer = document.getElementById('answerQ'+i)
-        const questionBox = document.getElementById('question'+i)
+        const result = document.querySelector('input[name="q'+i+'"]:checked')   //Pour chaque question, on vient chercher l'input radio qui est checked
+        const pAnswer = document.getElementById('answerQ'+i)       //Cela cible le p caché dans lequel on affichera bonne ou mauvaise réponse
+        const questionBox = document.getElementById('question'+i)  //Là on cible la box de la question pour pouvoir la changer de couleur en fonction de la réponse
         if (result.value == responses[i - 1]) {
             questionBox.classList = 'question good'
             pAnswer.textContent = 'Bonne réponse !'
@@ -19,7 +19,7 @@ function validate() {
             badAnswer+=1
         }  
     }
-    switch(badAnswer) {
+    switch(badAnswer) {     //Affichage final en fonction du nombre de mauvaises réponses
         case 0 :
             info.innerHTML = '<p>Wow quel résultat ! '+emojis[1]+'</p><p>Score : <span class="bold">'+(5 - badAnswer)+'/5</span></p>'
             body.classList.add('goat')
@@ -30,5 +30,5 @@ function validate() {
         default :
             info.innerHTML = '<p>'+emojis[2]+' Il reste quelques erreurs '+emojis[3]+'</p><p>Score : <span class="bold">'+(5 - badAnswer)+'/5</span></p><p>Retentez une autre réponse dans les cases rouges, puis re-validez !</p>'
     }
-    info.classList.add('column')
+    info.classList.add('column')    //Nécessaire pour le CSS
 }
